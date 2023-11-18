4c30dc66184d5f4a533ca8cf2672784d49fb84b7
we sell insurance <neelesh.poli2006@gmail.com>
2023-11-17 21:36:07 -0600

    Add end call and support
    User now knows when the call is ended as well. The will know when the call is ended. The issue of "Added to queue" message being shown after the user in sent to a call is known and a fix is being worked on.

--- src/calling/mod.rs
+++ src/calling/mod.rs
@@ -1,42 +1,182 @@
 use std::collections::HashMap;
 
+use poise::serenity_prelude as serenity;
+
+use crate::{Context, Error};
+
 pub struct Calling {
-    waiting_server: Option<u64>,
-    current_conversations: HashMap<u64, u64>
+    // The key is the channel id of channel, the value is the guild id (to make sure that the same server isn't paired togther if the same command is ran in a different channel)
+    waiting_servers: HashMap<u64, u64>,
+    current_conversations: HashMap<u64, u64>,
 }
 
 impl Calling {
     pub async fn new() -> Self {
         Self {
-            waiting_server: None,
-            current_conversations: HashMap::new()
+            waiting_servers: HashMap::new(),
+            current_conversations: HashMap::new(),
         }
     }
 
-    pub async fn add_to_queue(&mut self, channel_id: u64) -> Result<(), ()> {
-        if self.channel_in_queue(&channel_id).await {
+    pub async fn add_to_queue(
+        &mut self,
+        channel_id: u64,
+        guild_id: u64,
+        ctx: &Context<'_>,
+    ) -> Result<(), ()> {
+        if self.contains_channel(&channel_id).await {
             return Err(());
-        } else if self.waiting_server.is_some() {
-            tokio::task::block_in_place(|| {
-                self.current_conversations.insert(channel_id.to_owned(), self.waiting_server.as_ref().unwrap().to_owned());
-                self.current_conversations.insert(self.waiting_server.as_ref().unwrap().to_owned(), channel_id);
-            });
         } else {
-            self.waiting_server = Some(channel_id);
+            let mut found_channel_id: Option<u64> = None;
+            for (key, value) in self.waiting_servers.iter().clone() {
+                if value == &guild_id {
+                    continue;
+                }
+
+                found_channel_id = Some(*key);
+                self.current_conversations.insert(*key, channel_id);
+                self.current_conversations.insert(channel_id, *key);
+                break;
+            }
+
+            if found_channel_id.is_some() {
+                self.waiting_servers
+                    .remove_entry(&found_channel_id.unwrap());
+                // Send a message to both channels to let them know that the call is ready
+                ctx.say("Call ready! Beware of people trying to scam! They might go as far as being friends with you only to scam you later.").await.expect("Error sending message");
+
+                let channel = serenity::model::id::ChannelId(
+                    self.retrieve_channel_id(&channel_id)
+                        .await
+                        .unwrap()
+                        .to_owned(),
+                );
+                channel.say(&ctx.http(), "Call ready! Beware of people trying to scam! They might go as far as being friends with you only to scam you later.").await.expect("Error sending message");
+            } else {
+                self.waiting_servers.insert(channel_id, guild_id);
+            }
         }
         Ok(())
     }
 
-    pub async fn channel_in_queue(&self, &channel_id: &u64) -> bool {
-        tokio::task::block_in_place(|| {
-            self.current_conversations.contains_key(&channel_id)
-        })
+    pub async fn channel_in_convo(&self, &channel_id: &u64) -> bool {
+        self.current_conversations.contains_key(&channel_id)
     }
 
-
-    pub async fn retrieve_channel_id(&self, channel_id: &u64) -> Option<u64> {
-        tokio::task::block_in_place(|| {
-            self.current_conversations.get(&channel_id)
-        }).copied()
+    pub async fn contains_channel(&self, &channel_id: &u64) -> bool {
+        self.waiting_servers.contains_key(&channel_id)
+            || self.current_conversations.contains_key(&channel_id)
     }
+
+    pub async fn retrieve_channel_id(&self, channel_id: &u64) -> Option<&u64> {
+        self.current_conversations.get(&channel_id)
+    }
+
+    pub async fn remove_channel(
+        &mut self,
+        channel_id: &u64,
+        ctx: &Context<'_>,
+    ) -> Result<(), Error> {
+        match self.current_conversations.remove(channel_id) {
+            Some(id) => {
+                self.current_conversations.remove(&id);
+                ctx.say("Ended Conversation!").await?;
+
+                let channel = serenity::model::id::ChannelId(
+                    id
+                );
+                channel.say(&ctx.http(), "Ended Conversation!").await?;
+
+                Ok(())
+            }
+            None => match self.waiting_servers.remove(channel_id) {
+                Some(_) => {
+                    ctx.say("Removed from queue!").await?;
+                    Ok(())
+                }
+                None => {
+                    ctx.say("This channel is not in the queue or in a conversation!")
+                        .await?;
+                    Ok(())
+                }
+            },
+        }
+    }
+}
+
+/// Call other users on different servers
+///
+/// Running this command will place you into a queue and if ideal server is found, it enter a call
+#[poise::command(slash_command, subcommands("join", "exit"))]
+pub async fn call(_ctx: Context<'_>) -> Result<(), Error> {
+    Ok(())
+}
+
+#[poise::command(slash_command)]
+pub async fn join(ctx: Context<'_>) -> Result<(), Error> {
+    let mut calling = ctx.data().calling.write().await;
+    match calling
+        .add_to_queue(ctx.channel_id().0, ctx.guild_id().unwrap().0, &ctx)
+        .await
+    {
+        Ok(_) => {}
+        Err(_) => {
+            ctx.say("This channel is alredy in the queue or in a conversation!")
+                .await?;
+            return Ok(());
+        }
+    }
+
+    // Create a button that allows the user to leave the queue
+    // let exit_queue_button = serenity::CreateButton::default()
+    //     .label("Exit Queue")
+    //     .style(serenity::ButtonStyle::Danger)
+    //     .custom_id("exit_queue")
+    //     .to_owned();
+
+    // let action_row = serenity::CreateActionRow::default().add_button(exit_queue_button).to_owned();
+
+    let m = ctx
+        .send(
+            |m| m.content("Added to queue!"), // .components(|c| c.add_action_row(action_row))
+        )
+        .await?;
+
+    // let interaction = m
+    //     .message()
+    //     .await?
+    //     .await_component_interaction(ctx)
+    //     .timeout(std::time::Duration::from_secs(60))
+    //     .author_id(ctx.author().id)
+    //     .await
+    //     .unwrap();
+
+    // let exit_queue_button = serenity::CreateButton::default()
+    //     .label("Exit Queue")
+    //     .style(serenity::ButtonStyle::Danger)
+    //     .custom_id("exit_queue")
+    //     .disabled(true)
+    //     .to_owned();
+
+    // let action_row = serenity::CreateActionRow::default().add_button(exit_queue_button).to_owned();
+
+    // m.edit(ctx, |b| {
+    //     b.components(|b| b)
+    //         .content("Removed from queue!")
+    //         .components(|c| c.add_action_row(action_row))
+    // }).await?;
+
+    Ok(())
+}
+
+#[poise::command(slash_command)]
+pub async fn exit(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.data()
+        .calling
+        .write()
+        .await
+        .remove_channel(&ctx.channel_id().0, &ctx)
+        .await
+        .unwrap();
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,27 @@
 mod calling;
-mod logger;
 mod config;
+mod logger;
 
 use logger::LogType;
 use poise::serenity_prelude as serenity;
-use tokio::sync::{RwLock, Mutex};
-// Make custom types that are used, dw too much about this
-type Error = Box<dyn std::error::Error + Send + Sync>;
-type Context<'a> = poise::Context<'a, Data, Error>;
+use tokio::sync::{Mutex, RwLock};
+
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+pub type Context<'a> = poise::Context<'a, Data, Error>;
 // Used to store common data across all commands or data the commands might need to access that needs to persist between invocations
-struct Data {
+pub struct Data {
     logger: Mutex<logger::Logger>,
-    calling: RwLock<calling::Calling>
+    calling: RwLock<calling::Calling>,
 }
 
 /// Respond with pong
 #[poise::command(slash_command, prefix_command)]
-async fn ping(
-    // Use the custom type defined above
-    ctx: Context<'_>,
-) -> Result<(), Error> {
-    ctx.say(format!("Pong! Current latency: {}", ctx.ping().await.as_millis())).await?;
-    Ok(())
-}
-
-// These are docstrings, they are used to describe the command, which shows on discord too
-/// Call a user on a different server!
-#[poise::command(slash_command, prefix_command)]
-async fn call(
-    ctx: Context<'_>,
-) -> Result<(), Error> {
-    let mut calling = ctx.data().calling.write().await;
-    match calling.add_to_queue(ctx.channel_id().0).await {
-        Ok(_) => {},
-        Err(_) => {
-            ctx.say("This channel is alredy in the queue or in a conversation!").await?;
-            return Ok(());
-        }
-    }
-
-    // Create a button that allows the user to leave the queue
-    let exit_queue_button = serenity::CreateButton::default()
-        .label("Exit Queue")
-        .style(serenity::ButtonStyle::Danger)
-        .custom_id("exit_queue")
-        .to_owned();
-    
-    let action_row = serenity::CreateActionRow::default().add_button(exit_queue_button).to_owned();
-    
-
-    let _m = ctx.send(|m| m
-        .content("Added to queue!")
-        // .components(|c| c.add_action_row(action_row))
-    ).await?;
-
-    // let interaction = m
-    //     .message()
-    //     .await?
-    //     .await_component_interaction(ctx)
-    //     .author_id(ctx.author().id)
-    //     .await
-    //     .unwrap();
-
-    // m.edit(ctx, |b| {
-    //     b.components(|b| b).content("Removed from queue!")
-    // }).await?;
-
+async fn ping(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.say(format!(
+        "Pong! Current latency: {}",
+        ctx.ping().await.as_millis()
+    ))
+    .await?;
     Ok(())
 }
 
@@ -73,22 +29,40 @@ async fn event_handler(
     ctx: &serenity::Context,
     event: &poise::Event<'_>,
     _framework: poise::FrameworkContext<'_, Data, Error>,
-    data: &Data
+    data: &Data,
 ) -> Result<(), Error> {
     match event {
         poise::Event::Ready { data_about_bot } => {
-            data.logger.lock().await.log(LogType::Info, &format!("Bot is ready! Logged in as {}!", data_about_bot.user.name)).await?;
-        },
+            data.logger
+                .lock()
+                .await
+                .log(
+                    LogType::Info,
+                    &format!("Bot is ready! Logged in as {}!", data_about_bot.user.name),
+                )
+                .await?;
+        }
         poise::Event::Message { new_message } => {
             if new_message.is_own(&ctx.cache) {
                 return Ok(());
             }
-            let channel_string = new_message.channel_id.0;
+            let channel_id = new_message.channel_id.0;
             let calling = data.calling.read().await;
-            
-            if calling.channel_in_queue(&channel_string).await {
-                let channel = serenity::model::id::ChannelId(calling.retrieve_channel_id(&new_message.channel_id.0).await.unwrap());
-                channel.say(&ctx.http, format!("{}: {}", new_message.author.name, new_message.content)).await?;
+
+            if calling.channel_in_convo(&channel_id).await {
+                let channel = serenity::model::id::ChannelId(
+                    calling
+                        .retrieve_channel_id(&channel_id)
+                        .await
+                        .unwrap()
+                        .to_owned(),
+                );
+                channel
+                    .say(
+                        &ctx.http,
+                        format!("{}: {}", new_message.author.name, new_message.content),
+                    )
+                    .await?;
             }
         }
         _ => {}
@@ -96,49 +70,40 @@ async fn event_handler(
     Ok(())
 }
 
-// Tokio is a async runtime for Rust
 #[tokio::main]
 async fn main() {
     let options = poise::FrameworkOptions {
         // Put all commands that need to be registered here
         commands: vec![
             ping(),
-            call(),
+            calling::call(),
         ],
-        // Prefix here
         prefix_options: poise::PrefixFrameworkOptions {
             prefix: Some("--".to_string()),
-            // Use the defaults for the rest of the struct
             ..Default::default()
         },
         event_handler: |ctx, event, framework, data| {
             Box::pin(event_handler(ctx, event, framework, data))
         },
-        // Use the defaults for the rest of the struct
         ..Default::default()
     };
 
     poise::Framework::builder()
-        // Discord Bot Token
         .token(&config::CONFIG.as_ref().token)
-        // Initilize the data struct to be used when a command is called
         .setup(move |ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
                 Ok(Data {
                     logger: Mutex::new(logger::Logger::new().await?),
-                    calling: RwLock::new(calling::Calling::new().await)
+                    calling: RwLock::new(calling::Calling::new().await),
                 })
             })
         })
-        // Load the options here
         .options(options)
-        // Discord intents
-        .intents(serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT)
-        // Run the bot sharded, check discord api docs for more info
+        .intents(
+            serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT,
+        )
         .run_autosharded()
-        // Keep the program awaiting here as other it will terminate with the bot running
         .await
-        // Used to get the result value (if its an error or not)
         .unwrap();
 }

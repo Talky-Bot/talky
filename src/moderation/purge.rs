35cac91d44f1517a81d710553f9271e97884e0cc
we sell insurance <neelesh.poli2006@gmail.com>
2024-04-16 21:24:05 -0500

    Added purge any command to remove specified about of messages

--- src/moderation/purge.rs
+++ src/moderation/purge.rs
@@ -5,7 +5,7 @@ use crate::{error::BotError, Context};
 #[poise::command(
     slash_command,
     guild_only,
-    subcommands("after"),
+    subcommands("after", "any"),
     default_member_permissions = "MANAGE_MESSAGES"
 )]
 pub async fn purge(_ctx: Context<'_>, _amount: u64) -> Result<(), BotError> {
@@ -16,8 +16,7 @@ pub async fn purge(_ctx: Context<'_>, _amount: u64) -> Result<(), BotError> {
 #[poise::command(slash_command, ephemeral)]
 async fn after(
     ctx: Context<'_>,
-    #[description = "The ID of the message to delete after"]
-    message_id: serenity::MessageId,
+    #[description = "The ID of the message to delete after"] message_id: serenity::MessageId,
     #[description = "The amount of messages to delete. Must be less than 100. Defaults to 100."]
     #[min = 1]
     #[max = 100]
@@ -26,6 +25,31 @@ async fn after(
     ctx.defer_ephemeral().await?;
     let messages = GetMessages::new()
         .after(message_id)
+        .limit(amount.unwrap_or(100))
+        .execute(ctx.http(), ctx.channel_id())
+        .await?;
+
+    for message in &messages {
+        message.delete(ctx.http()).await?;
+    }
+
+    ctx.say(format!("Deleted {} messages", &messages.len()))
+        .await?;
+
+    Ok(())
+}
+
+/// Deletes the specifed amount of messages. Defaults to 100 messages deleted.
+#[poise::command(slash_command, ephemeral)]
+async fn any(
+    ctx: Context<'_>,
+    #[description = "The amount of messages to delete. Must be less than 100. Defaults to 100."]
+    #[min = 1]
+    #[max = 100]
+    amount: Option<u8>,
+) -> Result<(), BotError> {
+    ctx.defer_ephemeral().await?;
+    let messages = GetMessages::new()
         .limit(amount.unwrap_or(100))
         .execute(ctx.http(), ctx.channel_id())
         .await?;
